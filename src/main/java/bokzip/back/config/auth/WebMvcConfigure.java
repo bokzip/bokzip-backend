977b3390e02b08d414c817af9a37a5daac0c8329
eunseo2 <20180972@sungshin.ac.kr>
2021-11-27 12:45:46 +0900

    [fix:#60] 로그아웃

--- src/main/java/bokzip/back/config/auth/WebMvcConfigure.java
+++ src/main/java/bokzip/back/config/auth/WebMvcConfigure.java
@@ -9,6 +9,5 @@ public class WebMvcConfigure implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/logoutPage").setViewName("logout");
     }
 }
